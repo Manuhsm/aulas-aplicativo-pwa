1018bb2bc9b39940a3acc457655a08c2a0b45431
Manuhsm <manuelahsm167@gmail.com>
2026-03-17 22:27:59 -0300

    first commit

---
+++ src/componentes/BotaoCustomizado/BotaoCustomizado.jsx
@@ -0,0 +1,17 @@
+import "./BotaoCustomizado.css";
+
+function BotaoCustomizado({ tipo, children }) {
+    let classes = "botao-customizado__root";
+
+    switch (tipo) {
+        case "primario":
+            classes += " botao-customizado__primario";
+            break;
+        case "segundario":
+            classes += " botao-customizado__segundario";
+            break;
+    }
+    return <button className={classes}>{children}</button>
+}
+
+export default BotaoCustomizado;
