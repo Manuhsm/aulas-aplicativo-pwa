67731355cf93c7a9079534ac892830bbb3e281f0
Manuhsm <manuelahsm167@gmail.com>
2026-03-18 20:24:11 -0300

    salvando

--- src/paginas/PaginaInicial/PaginaInicial.jsx
+++ src/paginas/PaginaInicial/PaginaInicial.jsx
@@ -1,12 +1,24 @@
 import BotaoCustomizado from "../../componentes/BotaoCustomizado/BotaoCustomizado";
 import Principal from "../../componentes/Principal/Principal";
 import "./PaginaInicial.css";
+import { useNavigate } from 'react-router-dom';
 
 function PaginaInicial() {
+  const navigate = useNavigate();
+
+
+
+
   return (
   <Principal>
-    <BotaoCustomizado tipo="primario">Salvar</BotaoCustomizado>
-    <BotaoCustomizado tipo="segundario">Cancelar</BotaoCustomizado>
+    <BotaoCustomizado tipo="primario" aoClicar={() => navigate('/lista-produtos')}>
+      Lista de Produtos
+      </BotaoCustomizado>
+
+    <BotaoCustomizado tipo="segundario" aoClicar={() => navigate('/blabla')}>
+      Rota invalidada
+      </BotaoCustomizado>
+      
   </Principal>
   );
 }
